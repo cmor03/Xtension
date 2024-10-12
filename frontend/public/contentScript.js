2f7322d376fd8be52c1cc92d03a0671a21b27c75
cmor03 <coltonhmorris@gmail.com>
2024-10-12 14:36:01 -0700

    update

--- frontend/public/contentScript.js
+++ frontend/public/contentScript.js
@@ -7,8 +7,7 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     console.log("Sending page content:", content.substring(0, 100) + "...");
     sendResponse({ content: content });
   }
-  return true; // Indicates that the response will be sent asynchronously
+  return true;
 });
 
-// Notify that the content script has been injected
 chrome.runtime.sendMessage({ action: "contentScriptInjected" });
